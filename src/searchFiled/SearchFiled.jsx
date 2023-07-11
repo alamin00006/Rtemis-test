d7810d06fbf07122f14b285460bf844472a49fb4
Mohammad Al Amin <alaminbamna08@gmail.com>
2023-07-11 12:18:23 +0600

    search filed done

--- src/searchFiled/SearchFiled.jsx
+++ src/searchFiled/SearchFiled.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./SearchFiled.css";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { BsArrowRight } from "react-icons/bs";
 const SearchFiled = () => {
   return (
-    <div className="search-filed d-flex justify-content-center">
-      <div className="border border-danger ">
-        <ul className="d-flex main-search">
+    <div className="container search-filed d-flex justify-content-center">
+      <div style={{ border: "1px solid #00bbb4" }}>
+        <ul className="d-flex main-search mt-5">
           <li>
             <a href="...">Daily</a>
           </li>
@@ -38,6 +40,68 @@ const SearchFiled = () => {
             <a href="...">Others</a>
           </li>
         </ul>
+        <div className="input-filed-area">
+          <input type="text" style={{ width: "100%", height: "40px" }} />
+          <div className="location-icon ">
+            <MdOutlineLocationOn
+              style={{ color: "#00bbb4", width: "20px", height: "20px" }}
+            />
+          </div>
+          <div className="left-date">
+            <input type="date" />
+          </div>
+          <div className="right-date">
+            <input type="date" />
+          </div>
+          <div class="vl"></div>
+          <div className="arrow-icon">
+            <BsArrowRight />
+          </div>
+        </div>
+        {/* Rent Styled Searching */}
+        <ul className="d-flex styled-search-1 mt-3">
+          <li>
+            <a href="...">All</a>
+          </li>
+          <li>
+            <a href="...">Studio</a>
+          </li>
+          <li>
+            <a href="...">1BR</a>
+          </li>
+          <li>
+            <a href="...">2BR</a>
+          </li>
+          <li>
+            <a href="...">3BR</a>
+          </li>
+        </ul>
+        <div className="d-flex justify-content-between align-items-center">
+          <ul className="d-flex styled-search-1 mt-3">
+            <li>
+              <a href="...">All</a>
+            </li>
+            <li>
+              <a href="...">Full Furnished</a>
+            </li>
+            <li>
+              <a href="...">Unfurnished</a>
+            </li>
+          </ul>
+          <div>
+            <button
+              style={{
+                backgroundColor: "#00bbb4",
+                border: "none",
+                color: "white",
+                padding: "5px 10px",
+                borderRadius: "5px",
+              }}
+            >
+              Find Accomodation
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
